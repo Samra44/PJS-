316212452a2a2872fe78c24089c760abccae664c
Samra44 <154986251+Samra44@users.noreply.github.com>
2024-08-26 18:13:52 +0500

    Add files via upload
    PJS FILES

---
+++ PJS-File-main/PJS/loops.js
@@ -0,0 +1,44 @@
+// let table =5;
+// for(let i =1;i<=10;i++){
+//     console.log(table + "X" +i+ "=" +(table*i));
+// }
+
+// object
+
+//  student_info = {
+//     name: "samra",
+//     age:    "20",
+//     skill:  "web developer",
+//     }
+
+// for in
+
+    // for (const key in student_info){
+    //     console.log(key + ":" + student_info[key]);
+    // }
+
+    // for of
+    
+    // let arr = ["apple","Banana","Mango","Orange","Grapes"]
+    // for (const i of arr) {
+    //     console.log(i)
+    // }
+
+    for (const i of "Samrah Anwar"){
+      console.log(i)
+    }
+
+    // Nested if
+    
+    // let a =1;
+    // let b = "2"
+    // if (a === 1){
+    // if (a== 2) {
+    //    console.log("a is 1 and b is 2");}
+    //    else{console.log("a is note 1");
+
+    //    }
+    // } 
+    
+
+
